09a28a6345b7cbac66c3acf1625e83b08897fbb9
Tang Rufus <tangrufus@gmail.com>
2014-12-14 03:38:51 +0800

    Add uninstall script

--- uninstall.php
+++ uninstall.php
@@ -25,7 +25,49 @@
  * @package    Lazy_Disqus
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	exit;
+}
+
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
+}
+
+
+// Important: Check if the file is the one
+// that was registered during the uninstall hook.
+if ( 'lazy-disqus/lazy-disqus.php' !== WP_UNINSTALL_PLUGIN )  {
+	exit;
+}
+
+// Check if the $_REQUEST content actually is the plugin name
+if ( ! in_array( 'lazy-disqus/lazy-disqus.php', $_REQUEST['checked'] ) ) {
+	exit;
+}
+
+if ( 'delete-selected' !== $_REQUEST['action'] ) {
+	exit;
+}
+
+// Check user roles.
+if ( ! current_user_can( 'activate_plugins' ) ) {
+	exit;
+}
+
+// Run an admin referrer check to make sure it goes through authentication
+check_admin_referer( 'bulk-plugins' );
+
+// Safe to carry on
+if ( false !== get_option( 'lazy_disqus_enqueued_admin_notices' ) ) {
+	delete_option( 'lazy_disqus_enqueued_admin_notices' );
+}
+
+if ( false !== get_option( 'lazy_disqus_settings' ) ) {
+	delete_option( 'lazy_disqus_settings' );
+}
+
+if ( false !== get_option( 'lazy_disqus_version' ) ) {
+	delete_option( 'lazy_disqus_version' );
 }

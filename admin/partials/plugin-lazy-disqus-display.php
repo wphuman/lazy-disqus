19506f2b5cdd6173edb99e9d2725ec6999a326ea
Tang Rufus <tangrufus@gmail.com>
2014-12-14 01:24:50 +0800

    Set plugin name -> lazy disqus

--- admin/partials/plugin-lazy-disqus-display.php
+++ admin/partials/plugin-lazy-disqus-display.php
@@ -8,8 +8,8 @@
  * @link       http://example.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin/partials
+ * @package    Lazy_Disqus
+ * @subpackage Lazy_Disqus/admin/partials
  */
 ?>
 
